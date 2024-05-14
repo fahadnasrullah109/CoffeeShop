5ee7a586d139ea3ef680ae79f253ab459faaae52
fahadnasrullah <fahad@rayn.group>
2024-05-14 12:33:13 +0500

    feat: Sizes selected added.

--- app/src/main/java/com/coffee/shop/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/coffee/shop/presentation/detail/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.coffee.shop.presentation.detail
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,12 +17,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -54,36 +61,36 @@ import com.coffee.shop.utils.getDummyDomainCoffee
 
 @Composable
 fun DetailScreen(modifier: Modifier = Modifier, coffee: DomainCoffee, onBack: () -> Unit) {
-    Scaffold(modifier = modifier,
-        containerColor = appBgColor, topBar = {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton(onClick = onBack) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = null
-                    )
-                }
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-                    fontWeight = FontWeight.SemiBold,
-                    text = stringResource(id = R.string.label_detail),
-                    style = TextStyle(color = textTitleColor, fontSize = 18.sp),
-                    fontFamily = soraFamily
+    var selectedSize by remember {
+        mutableIntStateOf(0)
+    }
+    Scaffold(modifier = modifier, containerColor = appBgColor, topBar = {
+        Row(
+            modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null
                 )
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_heart),
-                        contentDescription = null
-                    )
-                }
             }
-        }) { contentPadding ->
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                fontWeight = FontWeight.SemiBold,
+                text = stringResource(id = R.string.label_detail),
+                style = TextStyle(color = textTitleColor, fontSize = 18.sp),
+                fontFamily = soraFamily
+            )
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_heart),
+                    contentDescription = null
+                )
+            }
+        }
+    }) { contentPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -93,15 +100,28 @@ fun DetailScreen(modifier: Modifier = Modifier, coffee: DomainCoffee, onBack: ()
                 modifier = Modifier
                     .fillMaxSize()
                     .weight(1f)
-                    .verticalScroll(rememberScrollState()), coffee = coffee
+                    .verticalScroll(rememberScrollState()),
+                coffee = coffee,
+                selectedSize = selectedSize
+            ) {
+                if(selectedSize != it) {
+                    selectedSize = it
+                }
+            }
+            BuyNowRow(
+                modifier = Modifier.fillMaxWidth(), coffee = coffee, selectedSize = selectedSize
             )
-            BuyNowRow(modifier = Modifier.fillMaxWidth(), coffee = coffee)
         }
     }
 }
 
 @Composable
-private fun DetailContent(modifier: Modifier, coffee: DomainCoffee) {
+private fun DetailContent(
+    modifier: Modifier,
+    coffee: DomainCoffee,
+    selectedSize: Int,
+    onSelectedSizeChanged: (Int) -> Unit
+) {
     Column(
         modifier = modifier.padding(20.dp)
     ) {
@@ -112,8 +132,7 @@ private fun DetailContent(modifier: Modifier, coffee: DomainCoffee) {
                 .aspectRatio(2.0f)
                 .clip(RoundedCornerShape(16.dp)),
             model = ImageRequest.Builder(LocalContext.current)
-                .data("${AppConstant.GET_IMAGES_URL}${coffee.image}")
-                .crossfade(true).build(),
+                .data("${AppConstant.GET_IMAGES_URL}${coffee.image}").crossfade(true).build(),
             placeholder = painterResource(R.drawable.ic_launcher_background),
             contentDescription = null
         )
@@ -145,11 +164,13 @@ private fun DetailContent(modifier: Modifier, coffee: DomainCoffee) {
                 fontWeight = FontWeight.SemiBold
             )
         }
-        Spacer(modifier = Modifier
-            .fillMaxWidth()
-            .padding(all = 20.dp)
-            .height(1.dp)
-            .background(color = Color(0xFFEAEAEA)))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 20.dp)
+                .height(1.dp)
+                .background(color = Color(0xFFEAEAEA))
+        )
         Text(
             text = stringResource(id = R.string.label_description),
             style = TextStyle(color = textTitleColor, fontSize = 18.sp),
@@ -162,18 +183,78 @@ private fun DetailContent(modifier: Modifier, coffee: DomainCoffee) {
             style = TextStyle(color = textHomeGrayColor, fontSize = 14.sp),
             fontFamily = soraFamily
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = R.string.label_size),
+            style = TextStyle(color = textTitleColor, fontSize = 18.sp),
+            fontFamily = soraFamily,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            if (selectedSize == 0) {
+                SelectedSize(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), size = "S"
+                ) {
+                    onSelectedSizeChanged.invoke(0)
+                }
+            } else {
+                UnselectedSize(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), size = "S"
+                ) {
+                    onSelectedSizeChanged.invoke(0)
+                }
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            if (selectedSize == 1) {
+                SelectedSize(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), size = "M"
+                ) {
+                    onSelectedSizeChanged.invoke(1)
+                }
+            } else {
+                UnselectedSize(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), size = "M"
+                ) {
+                    onSelectedSizeChanged.invoke(1)
+                }
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            if (selectedSize == 2) {
+                SelectedSize(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), size = "L"
+                ) {
+                    onSelectedSizeChanged.invoke(2)
+                }
+            } else {
+                UnselectedSize(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), size = "L"
+                ) {
+                    onSelectedSizeChanged.invoke(2)
+                }
+            }
+        }
     }
 }
 
 @Composable
-private fun BuyNowRow(modifier: Modifier, coffee: DomainCoffee) {
+private fun BuyNowRow(modifier: Modifier, coffee: DomainCoffee, selectedSize: Int) {
     Row(
-        modifier = modifier
-            .background(
-                color = Color.White,
-                shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
-            ),
-        verticalAlignment = Alignment.CenterVertically
+        modifier = modifier.background(
+            color = Color.White, shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+        ), verticalAlignment = Alignment.CenterVertically
     ) {
         Column(
             Modifier
@@ -188,7 +269,15 @@ private fun BuyNowRow(modifier: Modifier, coffee: DomainCoffee) {
                 fontFamily = soraFamily
             )
             Text(
-                text = "$ ${coffee.priceSmall}",
+                text = "$ ${
+                    when (selectedSize) {
+                        0 -> coffee.priceSmall
+                        1 -> coffee.priceMedium
+                        else -> {
+                            coffee.priceLarge
+                        }
+                    }
+                }",
                 fontSize = 25.sp,
                 style = TextStyle(color = MaterialTheme.colorScheme.primary),
                 fontWeight = FontWeight.SemiBold,
@@ -196,12 +285,12 @@ private fun BuyNowRow(modifier: Modifier, coffee: DomainCoffee) {
             )
         }
 
-        CoffeeButton(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(55.dp)
-                .weight(.6f),
-            buttonTitle = stringResource(id = R.string.label_buy_now), onTap = { })
+        CoffeeButton(modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp)
+            .weight(.6f),
+            buttonTitle = stringResource(id = R.string.label_buy_now),
+            onTap = { })
         Spacer(modifier = Modifier.width(20.dp))
     }
 }
@@ -210,9 +299,64 @@ private fun BuyNowRow(modifier: Modifier, coffee: DomainCoffee) {
 @Composable
 private fun DetailScreenPreview() {
     CoffeeShopTheme {
-        DetailScreen(
-            modifier = Modifier.fillMaxSize(),
+        DetailScreen(modifier = Modifier.fillMaxSize(),
             coffee = getDummyDomainCoffee(),
             onBack = {})
+    }
+}
+
+@Composable
+private fun UnselectedSize(modifier: Modifier, size: String, onClick: (String) -> Unit) {
+    Button(border = BorderStroke(width = 1.dp, color = textHomeGrayColor),
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        modifier = modifier,
+        onClick = {
+            onClick.invoke(size)
+        }) {
+        Text(
+            text = size,
+            fontSize = 20.sp,
+            style = TextStyle(color = Color.Black),
+            fontFamily = soraFamily
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun UnselectedSizePreview() {
+    CoffeeShopTheme {
+        UnselectedSize(modifier = Modifier.fillMaxWidth(), size = "S", onClick = {})
+    }
+}
+
+@Composable
+private fun SelectedSize(modifier: Modifier, size: String, onClick: (String) -> Unit) {
+    Button(border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.primary),
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFFFF5EE)
+        ),
+        modifier = modifier,
+        onClick = {
+            onClick.invoke(size)
+        }) {
+        Text(
+            text = size,
+            fontSize = 20.sp,
+            style = TextStyle(color = MaterialTheme.colorScheme.primary),
+            fontFamily = soraFamily
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SelectedSizePreview() {
+    CoffeeShopTheme {
+        SelectedSize(modifier = Modifier.fillMaxWidth(), size = "M", onClick = {})
     }
 }
